7bc2d978592103669273d93e0712142d4780e755
mAC <ACmac@mACs-MacBook-Pro.local>
2018-04-07 20:44:57 -0400

    m

--- js/main.js
+++ js/main.js
@@ -5,9 +5,11 @@ ourRequest.open('GET', 'https://learnwebcode.github.io/json-example/animals-1.js
 ourRequest.onload = function() {
    // console.log(ourRequest.responseText) <-- for checking in console
 
-   //save retrieved data into a variable
+   //save retrieved data into a variable, "ourRequest.responseText" treats data as String from the site
+    //var ourData = ourRequest.responseText;
 
-   var ourData = ourRequest.responseText;
+    //below the requested URL will pass thru the JSON filter 
+    var ourData = JSON.parse(ourRequest.responseText);
 
    //if only wants to log out 1st object from the array
 
