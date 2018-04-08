f8f809249e58fdad414ca6d7bcc1457d0da679a7
mAC <ACmac@mACs-MacBook-Pro.local>
2018-04-07 20:37:08 -0400

    m

--- js/main.js
+++ js/main.js
@@ -3,7 +3,15 @@ var ourRequest = new XMLHttpRequest();
 ourRequest.open('GET', 'https://learnwebcode.github.io/json-example/animals-1.json');
 //what to do after established
 ourRequest.onload = function() {
-    console.log(ourRequest.responseText)
+   // console.log(ourRequest.responseText) <-- for checking in console
+
+   //save retrieved data into a variable
+
+   var ourData = ourRequest.responseText
+
+   //if only wants to log out 1st object from the array
+
+   console.log(ourData[0])
 
 };
 
