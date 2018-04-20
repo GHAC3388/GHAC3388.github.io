4795ebbd38cf3fea8cfd1cb4a4d7767e5e3dfa46
mAC <ACmac@mACs-MacBook-Pro.local>
2018-04-19 23:56:52 -0400

    z

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
 var element = document.getElementById('hide-block');
 
 Hammer(element).on("swipe", function(event) {
-  element.style.backgroundColor="blue";
+  element.style.display="none";
   
 });
 
